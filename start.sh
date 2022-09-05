d730d152a466e0b3a578b8db786e1ecb176245b9
narutooxox <112558957+narutooxox@users.noreply.github.com>
2022-09-06 00:26:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/haashim007/jkhh.git /EvaMaria
+  git clone https://github.com/narutooxox/Testing.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
